fb43eabc1bd40ca5d7c30eb99865252b964cd249
dionizio1903 <dionizio1903@gmail.com>
2024-07-30 18:20:35 -0400

    poo

--- curso_php/cadastro_aluno/cadastrar_aluno.php
+++ curso_php/cadastro_aluno/cadastrar_aluno.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <form action="?page=salvar" method="post" class=" col-5 h-3 w-30 mx-auto p-5">
 
 <input type="hidden" name="acao" value="cadastrar">
@@ -48,65 +47,8 @@
 </div>
 
 <button type="submit" class="btn btn-primary py-2 w-100">Enviar</button>
-=======
-
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
- <form action="?page=salvar" method="post" class=" col-5 h-3 w-30 mx-auto p-5">
-
-   <input type="hidden" name="acao" value="cadastrar">
-
-   <div class="mb-3" col>
-     <label for="">Nome</label>
-     <input type="text" name="nome" class="form-control">
-   </div>
-
-   <div class="mb-3">
-     <label for="">Cpf</label>
-     <input type="number" name="cpf" class="form-control">
-   </div>
-   <div class="mb-3">
-     <label for="" class="form-label">Data de nascimento</label>
-     <input type="date" name="data_nasc" class="form-control">
-   </div>
-
-   <div class="form-check form-check-inline mb-3 py-3  ">
-     <input class="form-check-input" type="radio" name="genero"  value="masculino">
-     <label class="form-check-label" for="inlineRadio1">Masculino</label>
-   </div>
-   <div class="form-check form-check-inline mb-3  ">
-     <input class="form-check-input" type="radio" name="genero"  value="feminino">
-     <label class="form-check-label " for="inlineRadio2">Feminino</label>
-   </div>
-
-   
-   
-  <select class="form-select form-select-lg mb-3 form-control" aria-label=".form-select-lg example" name="estado_civil" aria-label="Default select example">
-  <option selected>Estado civil</option>
-  <option name="estado_civil" value="solteiro">Soltero</option>
-  <option name="estado_civil" value="casado">Casado</option>
-  <option name="estado_civil" value="divorciado">Divorciado</option>
-  </select>
-
-
-
-   <div>
-     <label>Observações</label>
-     <textarea class="form-control mb-3" placeholder="Observações" id="floatingTextarea" name="obs"></textarea>
->>>>>>> a3dd44d2b9fe99ecf32273493a8eb30659d501e0
 
    </div>
 
-<<<<<<< HEAD
 
 
-=======
-   <button type="submit" class="btn btn-primary py-2 w-100">Enviar</button>
-
- </form>
-
- 
->>>>>>> a3dd44d2b9fe99ecf32273493a8eb30659d501e0
--- curso_php/calc_area/index.php
+++ curso_php/calc_area/index.php
@@ -3,22 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calcular a area do retangulo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   
 </head>
 <body>
-<form action="calc.php" method="post" class=" col-5 h-3 w-30 mx-auto p-5">    
-<div class="row g-3 align-items-center mb-3 py-5">
-  <div class="col-auto  mb-3">
-    <label for="" class="col-form-label mb-3">Altura</label>
-  
-    <input type="number" id="" class="form-control mb-3">
+
+<form class="col-5 h-3 w-30 mx-auto p-5" action="calc.php"  method="post">
+  <div class="row mb-3">
+    <label  class="col-sm-2 col-form-label">Altura</label>
+    <div class="col-sm-10">
+      <input type="number" name="altura" class="form-control" >
+    </div>
   </div>
-  <div class="col-auto mb-3 ">
-    <label for="" class="col-form-label">Largura</label>
+
+  <div class="row mb-3">
+    <label  class="col-sm-2 col-form-label">Largura</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" name="largura">
+    </div>
+  </div> 
   
-    <input type="number" id="" class="form-control" >
-  </div>
-  </form>
-</div>
+  <button type="submit" class="btn btn-primary" value="">Calcular</button>
+  
+
+</form>
 </body>
 </html>
